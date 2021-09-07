f0013a0d458c4743c2207001a5becbf670350778
Mira Ghaly <mira.ghaly@smokealarmsolutions.com.au>
2021-09-07 20:31:21 +1000

    Version 0.1

--- MG.Plugins.Email.InsertTemplate/InsertTemplate.cs
+++ MG.Plugins.Email.InsertTemplate/InsertTemplate.cs
@@ -22,7 +22,10 @@ namespace MG.Plugins.Email
             IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
             // Getting the service from the Organisation Service.
             IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
-
+            var emailDescription = "";
+            var templateId = Guid.Empty;
+            Entity emailEntityPreImage = null;
+            EntityReference regardingObjectId = null;
             try
             {
 
@@ -30,16 +33,55 @@ namespace MG.Plugins.Email
                 if (context.InputParameters.Contains("Target") &&
                     context.InputParameters["Target"] is Entity)
                 {
-                    //Get email
+                    tracingService.Trace("Entered Plugin");
+
                     Entity emailEntity = (Entity)context.InputParameters["Target"];
-                    tracingService.Trace(emailEntity.Id.ToString());
-                    emailEntity.Attributes["description"] = "Mira Updated";
-                    service.Update(emailEntity);
+                    if(context.MessageName=="Update")
+                         emailEntityPreImage = (Entity)context.PreEntityImages["Image"];
+                    if (emailEntity.Attributes.Contains("description"))
+                    {
+                        emailDescription = emailEntity.Attributes["description"].ToString();
+                    }
+                    else
+                    {
+                        if (context.MessageName == "Update")
+                            emailDescription = emailEntityPreImage.Attributes["description"].ToString();
+                    }
+                    if (emailEntity.Attributes.Contains("tg_emailtemplate"))
+                    {
+                        templateId = ((EntityReference)emailEntity.Attributes["tg_emailtemplate"]).Id;
+                    }
+                    else
+                    {
+                        if(context.MessageName=="Update" && emailEntityPreImage.Attributes.Contains("tg_emailtemplate"))                      
+                            templateId = ((EntityReference)emailEntityPreImage.Attributes["tg_emailtemplate"]).Id;
+                    }
+                    tracingService.Trace("Entered Plugin+POINT 0");
+                    if ((emailEntity.Attributes.Contains("regardingobjectid") || (context.MessageName=="Update" && emailEntityPreImage.Attributes.Contains("regardingobjectid"))) && templateId!=Guid.Empty)
+                    {
+                        if(emailEntity.Attributes.Contains("regardingobjectid"))
+                          regardingObjectId = (EntityReference)emailEntity.Attributes["regardingobjectid"];
+                        else if (context.MessageName == "Update" && emailEntityPreImage.Attributes.Contains("regardingobjectid"))
+                            regardingObjectId = (EntityReference)emailEntityPreImage.Attributes["regardingobjectid"];
+                        tracingService.Trace("Point 1");
+                        EntityReference primaryEntity = new EntityReference(regardingObjectId.LogicalName,regardingObjectId.Id);
+                        tracingService.Trace("Point 2");
+
+                        //Get Email template
+                        Entity emailTemplate = service.Retrieve("tg_emailtemplates", templateId, new ColumnSet("tg_emaildescription", "tg_emailsubject"));
+
+                        tracingService.Trace("Point 3");
+
+                        string description = MG.Generic.Helper.MessageHelper.GetMessageWithValues(primaryEntity, emailTemplate.Attributes["tg_emaildescription"].ToString(), service, tracingService);
+                        tracingService.Trace("Point 4");
+                        emailEntity.Attributes["description"] = description + emailDescription;
+                    }
+                  
                 }
             }
             catch (Exception ex)
             {
-                tracingService.Trace("Email_IsReply_SetTo: {0}", ex.ToString());
+                tracingService.Trace("Insert Template: {0}", ex.ToString());
                 throw new InvalidPluginExecutionException(ex.Message);
             }
         }
--- tg_InsertTemplate.js
+++ tg_InsertTemplate.js
@@ -1,5 +1,4 @@
 // JavaScript source code
-
 function InsertTemplate(executionContext) {
 
     var emailtemplateid = formContext.getAttribute("tg_emailtemplateid").getValue()[0].id.replace("}", "").replace("{", "").trim();
@@ -16,3 +15,76 @@ function InsertTemplate(executionContext) {
         }
     );
 }
+function OpenInsertDialog(formContext) {
+    debugger;
+    var emailid;
+
+    if (formContext.getAttribute("regardingobjectid").getValue() != null) {
+        if (formContext.ui.getFormType() == 1) {
+            formContext.data.save("save").then(
+                function () {
+                    emailid = formContext.data.entity.getId().replace(/[{}]/g, '');
+                    var pageInput = {
+                        pageType: "custom",
+                        name: "cr2d2_inserttemplate_8315a",
+                        entityName: "email",
+                        recordId: emailid
+                    };
+                    var navigationOptions = {
+                        target: 2,
+                        width: 700, // value specified in pixel
+                        height: 600, // value specified in pixel
+                        position: 1
+                    };
+                    Xrm.Navigation.navigateTo(pageInput, navigationOptions).then(
+                        function success() {
+                            // Run code on success
+                            formContext.data.refresh(false).then(function () { }, function () { });
+
+                        },
+                        function error() {
+                            // Handle errors
+                        }
+                    );
+                },
+                function () { }
+
+
+            );
+        }
+        else {
+            emailid = formContext.data.entity.getId().replace(/[{}]/g, '');
+            var pageInput = {
+                pageType: "custom",
+                name: "cr2d2_inserttemplate_8315a",
+                entityName: "email",
+                recordId: emailid
+            };
+            var navigationOptions = {
+                target: 2,
+                width: 700, // value specified in pixel
+                height: 600, // value specified in pixel
+                position: 1
+            };
+            Xrm.Navigation.navigateTo(pageInput, navigationOptions).then(
+                function success() {
+                    // Run code on success
+                    formContext.data.refresh(false).then(function () { }, function () { });
+
+                },
+                function error() {
+                    // Handle errors
+                }
+            );
+        }
+
+
+    }
+    else {
+        Xrm.Utility.alertDialog("Regarding should be selected!");
+        return false;
+
+    }
+
+}
+
